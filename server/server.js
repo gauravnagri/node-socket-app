eb5cae25fdb41e50bbcfc9886c7b2677934b00a6
XebiaNetCore <nagri.gaurav@gmail.com>
2018-08-25 20:01:44 +0530

    Added socket.IO and event handling for connect/disconnect

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const Path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
+
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+
+const io = socketIO(server);
 
 app.use(express.static(Path.join(__dirname,"../public")));
 
-app.listen(port,()=>{
+io.on("connection",(socket) => {
+   console.log("User connected...");
+   socket.on("disconnect",()=>{
+       console.log("User disconnected");
+   });
+});
+
+server.listen(port,()=>{
     console.log(`The app is running on port ${port}`);
-})
+});
