0f3f8bc4411d53a5babd7ae87f7a1b16d434ef84
XebiaNetCore <nagri.gaurav@gmail.com>
2018-08-26 15:34:43 +0530

    createMessage to be emitted to all users

--- server/server.js
+++ server/server.js
@@ -21,7 +21,11 @@ io.on("connection",(socket) => {
    });
 
    socket.on("createMessage",(data) => {
-      console.log("Create Message",data);
+      io.emit("newMessage",{
+         from : data.from,
+         text : data.text,
+         createdAt : new Date().getTime()
+      });
    });
 
    socket.on("disconnect",()=>{
