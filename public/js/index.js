0ca0fb6d3305931404cce851630a740745120874
XebiaNetCore <nagri.gaurav@gmail.com>
2018-08-27 19:20:57 +0530

    Added form to display new messages as well as send messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,24 @@
-var socket = io();
-socket.on("connect",() => {
-    console.log("Connected to server");
-});
+$(document).ready(function(){
+    var socket = io();
+    socket.on("connect",() => {
+        console.log("Connected to server");
+    });
+    
+    socket.on("newMessage",(data) => {
+       var li = `<li>${data.from}:${data.text}</li>`;
+       $("#messages").append(li);
+    });
+    
+    socket.on("disconnect",()=>{
+        console.log("Server disconnected...");
+    });
 
-socket.on("newMessage",(data) => {
-   console.log("New Message",data);
-});
+    $("#messageForm").on("submit",function(event){
+      event.preventDefault();
 
-socket.on("disconnect",()=>{
-    console.log("Server disconnected...");
+      socket.emit("createMessage",{
+         from : "User",
+         text : $("#message").val()
+      });
+    });
 });
